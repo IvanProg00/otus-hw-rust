dc6b6991da4840aac5ad30afe7b72bca60d9cb97
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2022-05-07 13:15:13 +0200

    Fix cargo fmt

--- hw01/src/main.rs
+++ hw01/src/main.rs
@@ -5,8 +5,10 @@ fn main() {
     let mut Result = Vec::with_capacity(COUNT);
     for i in 1..=COUNT {
         let s = match (i % 3, i % 5) {
-            (0, 0) => String::from("FizzBuzz"), (0, _) => String::from("Fizz"),
-            (_, 0) => String::from("Buzz"), (_, _) => format!("{}", i)
+            (0, 0) => String::from("FizzBuzz"),
+            (0, _) => String::from("Fizz"),
+            (_, 0) => String::from("Buzz"),
+            (_, _) => format!("{}", i),
         };
         Result.push(s);
     }
